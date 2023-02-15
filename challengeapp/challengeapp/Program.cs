2cf27f3b18da6d153ca4e7b6827b666e27a90829
endiendi <endi_m@wp.pl>
2023-02-15 08:10:18 +0100

    Zadanie 4

--- challengeapp/challengeapp/Program.cs
+++ challengeapp/challengeapp/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, Andrzej");
+﻿string strName = "Ewa";
+int intAge = 33;
+bool bolGender = true;
+
+
+
+if (bolGender == true)
+{
+    if (intAge < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        if (strName == "Ewa" && intAge == 33)
+        {
+            Console.WriteLine("Ewa, lat 33");
+        }
+        else
+            Console.WriteLine("Kobieta w wieku " + intAge);
+    }
+}
+else
+{
+    if (intAge < 18)
+    {
+        Console.WriteLine("Niepełnoletni męszczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Męszczyzna w wieku " + intAge);
+    }
+}
+
+
