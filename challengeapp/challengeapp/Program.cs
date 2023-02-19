bc5f57714d97ab9ec242970224253052e415a3bb
endi <endi_m@wp.pl>
2023-02-19 11:46:21 +0100

    Zadanie 06 poprawione (gdy pracownicy uzyskają ten sam wynik)

--- challengeapp/challengeapp/Program.cs
+++ challengeapp/challengeapp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using challengeapp;
+using System.Reflection;
 
 Employee employee1 = new Employee("Monika", "Kowalska", 35);
 Employee employee2 = new Employee("Zuzia", "Makowska", 24);
@@ -14,36 +15,39 @@ employee2.AddScore(1);
 employee2.AddScore(1);
 employee2.AddScore(1);
 employee2.AddScore(1);
-employee2.AddScore(2);
+employee2.AddScore(1);
 
 employee3.AddScore(1);
+employee3.AddScore(-1);
+employee3.AddScore(2);
 employee3.AddScore(1);
 employee3.AddScore(1);
-employee3.AddScore(1);
-employee3.AddScore(3);
 
-var result1 = employee1.Result;
-var result2 = employee2.Result;
-var result3 = employee3.Result;
-
+var sameMax = new List<int> ();
 List<Employee> employees = new List<Employee>()
     {
     employee1, employee2, employee3
     };
-
 
 int maxResult = -1;
 Employee employeeWithMaxResult = null;
 
 foreach (var employee in employees)
 {
- Console.WriteLine(employee.Name + "\t" + employee.Lastname + "\twiek\t"  + employee.Age + "\t" + "Uzyskał wynik\t" + employee.Result);
-   
-    if (employee.Result > maxResult)
+    Console.WriteLine(employee.Name + "\t" + employee.Lastname + "\twiek\t" + employee.Age + "\t" + "Uzyskał wynik\t" + employee.Result);
+    
+ if (employee.Result > maxResult) 
     {
         maxResult = employee.Result;
-        employeeWithMaxResult = employee;
     }
+    
 }
-Console.WriteLine("\nNajlepszy wynik uzyskał pracownik:\n");
-Console.WriteLine(employeeWithMaxResult.Name + "\t" + employeeWithMaxResult.Lastname + "\twiek\t" + employeeWithMaxResult.Age + "\t" + "Uzyskał wynik\t" + employeeWithMaxResult.Result);
+Console.WriteLine("\nLista pracowników którzy uzyskali najlepszy wynik:\n");
+foreach (var employee in employees)
+    if (employee.Result == maxResult)
+    {
+        maxResult = employee.Result;
+        Console.WriteLine(employee.Name + "\t" + employee.Lastname + "\twiek\t" + employee.Age + "\t" + "Uzyskał wynik\t" + employee.Result);
+
+    }
+
