df6a3ec238ea8af1bc392ede273045466f61bf08
endi <endi_m@wp.pl>
2023-02-19 21:08:00 +0100

    Zadanie 08

--- challengeapp/Program.cs
+++ challengeapp/Program.cs
@@ -18,7 +18,7 @@ employee2.AddScore(1);
 employee2.AddScore(1);
 
 employee3.AddScore(1);
-employee3.AddScore(-1);
+employee3.AddScore(1);
 employee3.AddScore(2);
 employee3.AddScore(1);
 employee3.AddScore(1);
@@ -30,7 +30,6 @@ List<Employee> employees = new List<Employee>()
     };
 
 int maxResult = -1;
-Employee employeeWithMaxResult = null;
 
 foreach (var employee in employees)
 {
